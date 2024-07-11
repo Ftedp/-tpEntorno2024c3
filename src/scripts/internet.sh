535e986349f5a510b3ceb9c4737c631efbe75047
Julio Corradini <julcorradi@gmail.com>
2024-07-11 20:35:42 -0300

    Creando el archivo internet.sh

--- src/scripts/internet.sh
+++ src/scripts/internet.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-#Creamos rama Internet para trabajar
 # Este script simplemente debe chequear que haya conexión a internet.
 # Asegúrese de retornar un valor de salida acorde a la situación.
 # Puede que necesite modificar el archivo Dockerfile.
 
-echo Chequeo de Internet no implementado. && exit 1
+
+# Dirección ip o dominio a probar (Google DNS)
+TARGET="8.8.8.8"
+
+# Enviar un ping y verificar el resultado
+if ping -c 1 $TARGET &> /dev/null
+then
+ echo "Conexión a internet establecida"
+else
+ echo "No hay conexión a internet"
+fi
