588f759d24696f75a12d678c7f46f2b72b9137e2
Julio Corradini <julcorradi@gmail.com>
2024-07-11 20:46:44 -0300

    Creando el archivo descargar.sh

--- src/scripts/descargar.sh
+++ src/scripts/descargar.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Este script debe descargar una sola imagen desde internet en la carpeta actual.
@@ -5,4 +6,30 @@
 # El nombre del archivo deberá ser su suma de verificación y debe terminar en .jpg
 # Asegúrese de devolver un valor de salida acorde a la situación.
 
-echo Descarga de imágenes de internet no implementada. && exit 1
+CLASE="$1"
+
+CARPETA_DESTINO="../../imagenes"
+
+URL="https://image.pollinations.ai/prompt/a%20photo%20of%20a%20${CLASE}?seed=1"
+
+ARCHIVO_TEMPORAL="${CARPETA_DESTINO}/${CLASE}_imagen_descargada.jpg"
+
+wget -O "$ARCHIVO_TEMPORAL" "$URL"
+
+
+if [ $? -eq 0 ]; then
+ CHECKSUM=$(sha256sum "$ARCHIVO_TEMPORAL" | awk '{ print $1 }')
+
+ mv "$ARCHIVO_TEMPORAL" "${CARPETA_DESTINO}/${CHECKSUM}.jpg"
+
+ echo "Imagen descargada con exitosamente."
+ 
+ echo 0
+
+else
+
+ echo "Hubo un problema al descargar la imagen."
+
+ echo 1
+
+fi 
