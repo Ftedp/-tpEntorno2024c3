ead3f661e6287d31dc34608f4c463880302846b6
Ftedp <facundo.torino7@gmail.com>
2024-07-18 22:16:17 +0000

    Corrigiendo archivo etiquetar.sh

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ ENV TERM=xterm
 ENV COLORTERM=24bit
 COPY ["src/", "/app/"]
 COPY ["src/scripts/", "/app/"]
-RUN chmod +x /app/scripts/descargar.sh
+RUN chmod +x /app/scripts/descargar.sh 
+RUN chmod +x /app/scripts/etiquetar.sh 
 WORKDIR /app
 ENTRYPOINT ["/app/main.sh"]
--- src/scripts/mostrar.sh
+++ src/scripts/mostrar.sh
@@ -4,4 +4,12 @@
 # Debe preguntarle al usuario que etiqueta desea buscar y mostrar por
 # pantalla todas las imágenes que tengan esa etiqueta.
 
+read -p "Ingrese la etiqueta que desea buscar: " etiqueta
+
+# Los archivos  .tag  contienen las etiquetas.
+for 
+
+
+
+
 echo Modulo de busqueda no implementado. && exit 1
