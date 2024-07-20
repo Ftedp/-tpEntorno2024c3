cdcdc7889b953c2a97af9fcbe181ad00ac0e8867
Ftedp <facundo.torino7@gmail.com>
2024-07-20 17:32:50 +0000

    Creando archivo mostrar.sh

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@
 INCLUDE+ .Dockerfile.base
 
 # Instalar los programas necesarios
-
+RUN apt-get update && update apt-get install -y wget iputils-ping coreutils jp2a
 
 # Configuracion de la aplicación
 ENV TERM=xterm
 ENV COLORTERM=24bit
 COPY ["src/", "/app/"]
+COPY ["src/scripts/", "/app/"]
+Run chmod +x /app/scripts/mostrar.sh
 WORKDIR /app
 ENTRYPOINT ["/app/main.sh"]
--- src/scripts/mostrar.sh
+++ src/scripts/mostrar.sh
@@ -4,4 +4,25 @@
 # Debe preguntarle al usuario que etiqueta desea buscar y mostrar por
 # pantalla todas las imágenes que tengan esa etiqueta.
 
-echo Modulo de busqueda no implementado. && exit 1
+read -p "Ingrese la etiqueta que desea buscar: " etiqueta
+imagenes=()
+
+for archivo in *.tag; do
+
+    if grep -q "$etiqueta" "$archivo"; then
+	imagenes+=("${archivo%.*}")
+    fi
+
+done
+
+if [ ${#imagenes[@]} -eq 0 ]; then
+    echo "No se encontro ninguna imagen con esta etiqueta." && exit 1
+fi
+
+echo "Imagenes encontradas."
+
+for imagen in "${imagenes[@]}"; do
+    echo "$imagen.jpg"
+done
+
+exit 0
